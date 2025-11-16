55a0b98300f12892fcd33c097f096fcfede54afd
usohrab18-ux <usohrab18@gmail.com>
2025-11-17 04:02:13 +0430

    Create ioi.py

---
+++ ioi.py
@@ -0,0 +1,11 @@
+python
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "ربات با موفقیت اجرا شد!"
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=10000)
