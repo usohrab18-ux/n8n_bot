0a4001bd22014b81a8580f38f90e6e5046b261f3
usohrab18-ux <usohrab18@gmail.com>
2025-11-17 04:29:26 +0430

    Update ioi.py

--- ioi.py
+++ ioi.py
@@ -5,7 +5,7 @@ app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return "ربات با موفقیت اجرا شد!"
+    return "Bot has started successfully!"
 
 if _name_ == '_main_':
     app.run(host='0.0.0.0', port=10000)
